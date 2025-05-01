ec93c3a6e0f3b6a45df31390a434eacaa695cab9
Pawan Gunawardhana <adpigunawardhana2@gmail.com>
2025-05-01 14:17:07 +0530

    Added react context and reducers to sync local workouts with database and created useWorkoutsContext.js adn WorkoutContext.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //pages and components
-import Home from './pages/Home' //importing Home page
-import Navbar from './components/Navbar';
+import Home from "./pages/Home"; //importing Home page
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <div className="pages">
           <Routes>
-            <Route
-              path="/"
-              element={<Home />} />
-        </Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
         </div>
-        
       </BrowserRouter>
-
     </div>
   );
 }
