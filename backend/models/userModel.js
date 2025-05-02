481ad1ea3d9ce3e43217977b5d3429a40ed2dd3a
Pawan Gunawardhana <adpigunawardhana2@gmail.com>
2025-05-02 20:45:00 +0530

    Defined user login and also created jwt for logged user.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const validator = require('validator');
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -18,22 +18,20 @@ const userSchema = new Schema({
 
 // static signup method
 userSchema.statics.signup = async function (email, password) {
-
-
   //validation
 
   if (!email || !password) {
-    throw Error ('All fields must be filled!')
+    throw Error("All fields must be filled!");
   }
 
   if (!validator.isEmail(email)) {
-    throw Error("Email is not valid")
+    throw Error("Email is not valid");
   }
 
   if (!validator.isStrongPassword(password)) {
-    throw Error ('Password is not strong enough')
+    throw Error("Password is not strong enough");
   }
-//email already exists
+  //email already exists
   const exists = await this.findOne({ email });
 
   if (exists) {
@@ -46,6 +44,25 @@ userSchema.statics.signup = async function (email, password) {
 
   const user = await this.create({ email, password: hash });
 
+  return user;
+};
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled!");
+  }
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("Email is incorrect");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Password is incorrect");
+  }
   return user;
 };
 
