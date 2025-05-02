13a828c131426c94bcab67c1de535c2763aad954
Pawan Gunawardhana <adpigunawardhana2@gmail.com>
2025-05-02 17:54:26 +0530

    Installed jsonwebtoken to create tokens.Used process called signing Tokens.Updated userController to work for tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,12 @@
 const User = require("../models/userModel");
 const mongoose = require("mongoose");
+const jwt = require('jsonwebtoken');
+
+//function for create tokens
+const createToken = (_id) => {
+  return jwt.sign({_id:_id}, process.env.SECRET, {expiresIn: '3d'})
+
+}
 
 // login
 const loginUser = async (req, res) => {
@@ -12,9 +19,12 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+    
+    //create a token
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(400).json({ error: error.message });
   }
 };
 
