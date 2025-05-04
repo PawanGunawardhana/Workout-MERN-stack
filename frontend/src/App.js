2f8f72eca8c05afdd4c848288581a14e83eb1d27
Pawan Gunawardhana <adpigunawardhana2@gmail.com>
2025-05-04 17:22:34 +0530

    Protected React routes for logged in and logged out users, redirect/navigate them accordingly for different pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 //pages and components
 import Home from "./pages/Home"; //importing Home page
@@ -7,6 +8,7 @@ import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
 function App() {
+  const { user } = useAuthContext();
   return (
     // Register Routes
     <div className="App">
@@ -14,9 +16,9 @@ function App() {
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={user ? <Home />: <Navigate to="/login"/>} />
+            <Route path="/signup" element={!user?<Signup />:<Navigate to="/"/>} />
+            <Route path="/login" element={!user?<Login />:<Navigate to="/"/> } />
           </Routes>
         </div>
       </BrowserRouter>
